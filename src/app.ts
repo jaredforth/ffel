2210fef0ba4734a06b4d54a4f5c182160ed53ce6
Jared Forth <jaredforthdev@gmail.com>
2020-09-22 09:25:09 -0500

    working constructing a class

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,58 @@
 import sgMail from '@sendgrid/mail';
 
-if (process.env.SENDGRID_API_KEY) {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-}
+
 
 // TODO update this with environment vars
 const msg = {
     to: 'jforth22@gmail.com',
     from: 'jared.forth@rivalmind.com', // Use the email address or domain you verified above
     subject: 'Sending with Twilio SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    text: 'and easy to do anywhere, even with Node.js'
 };
-//ES6
-sgMail
-    .send(msg)
-    .then(() => {console.log('sending email')}, error => {
-        console.error(error);
 
-        if (error.response) {
-            console.error(error.response.body)
+// This is the configuration for SendGrid
+interface Config {
+    to: string;
+    from: string;
+    subject: string;
+    text: string;
+}
+
+export class Logger implements Config {
+    to: string;
+    from: string;
+    subject: string;
+    text: string;
+
+    public constructor(config: Config) {
+        Logger.setApiKey();
+
+        this.to = config.to;
+        this.from = config.from;
+        this.subject = config.subject;
+        this.text = config.text;
+    }
+
+    public log(message: string) : void {
+        this.text = message;
+        sgMail
+            .send(msg)
+            .then(() => {console.log('sending email')}, error => {
+                console.error(error);
+
+                if (error.response) {
+                    console.error(error.response.body)
+                }
+            });
+    }
+
+    private static setApiKey() : boolean {
+        if (process.env.SENDGRID_API_KEY) {
+            sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+            return true;
+        } else {
+            console.error("SendGrid API key must be set");
+            return false;
         }
-    });
+    }
+}
